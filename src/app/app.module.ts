37e9aa96be0f81fc20e7c86aba8d17b71a4f23b2
Panos Bozas <fbozas@hotmail.com>
2022-09-05 20:26:34 +0300

    Create a custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoursesComponent } from 'src/courses.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
