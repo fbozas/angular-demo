bf43fe8237ce3a90d4287bd899642732517dc20d
Panos Bozas <fbozas@hotmail.com>
2022-09-05 20:41:33 +0300

    Generating Components using Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CoursesComponent } from 'src/courses.component';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
