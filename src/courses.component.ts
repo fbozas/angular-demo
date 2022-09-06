d0236a15359f6da6ad96c25c95b3651027ac737c
Panos Bozas <fbozas@hotmail.com>
2022-09-06 20:05:06 +0300

    Style Binding

--- src/courses.component.ts
+++ src/courses.component.ts
@@ -4,8 +4,8 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector : 'courses',
     template: `
-    <button class="btn btn-primary" [class.active] = "isActive">Submit</button>`
+    <button class="btn btn-primary" [style.backgroundColor] = "isActive ? 'blue': 'red'">Submit</button>`
 })
 export class CoursesComponent{
-   isActive = false;
+   isActive = true;
 }
