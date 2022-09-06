55f8828ce3d80dffd6317d6b7ecd59f89faf5436
Panos Bozas <fbozas@hotmail.com>
2022-09-06 20:25:49 +0300

    Template variables (simple)

--- src/courses.component.ts
+++ src/courses.component.ts
@@ -4,10 +4,10 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector : 'courses',
     template: `
-    <input (keyup) = "onKeyUp($event)" />`
+    <input (keyup.enter) = "onKeyUp($event)" />`
 })
 export class CoursesComponent{
     onKeyUp($event: any){
-        if($event.keyCode == 13) console.log("ENTER was pressed")
+       console.log($event.target.value);
     }
 }
