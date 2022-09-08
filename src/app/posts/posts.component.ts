2afc25dd44c2f16b6018c3807619a6dcf68aaeda
Panos Bozas <fbozas@hotmail.com>
2022-09-08 20:47:37 +0300

    HTTP Services: Creating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,11 +8,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PostsComponent implements OnInit {
   posts: any;
+  url = 'https://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: HttpClient) { 
-    http.get('https://jsonplaceholder.typicode.com/posts').subscribe(response =>{
+    http.get(this.url).subscribe(response =>{
       this.posts = response;
     });
+  }
+
+  createPost(input: HTMLInputElement){
+    let post = {title: input.value};
+    input.value = '';
+    this.http.post(this.url,post).subscribe(response => {
+      this.posts.unshift(post);
+      console.log(response);
+    })
   }
 
   ngOnInit(): void {
