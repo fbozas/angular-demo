84318359406e4c5136698fd4f02b92519f713d18
Panos Bozas <fbozas@hotmail.com>
2022-09-12 18:25:40 +0300

    CheckBoxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- canSave = true;
+  isChecked = false;
+
+  onChange($event: any){
+    console.log($event);
+  }
 }
