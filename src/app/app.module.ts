eb110fe4e3a17fbeb524814ec4486eee777fe03e
Panos Bozas <fbozas@hotmail.com>
2022-09-12 20:19:32 +0300

    Angular Material: Creating a reusable module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,19 +20,10 @@ import { TeamComponent } from './team/team.component';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
+
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import {MatIconModule} from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatComponentsModule } from './mat-components/mat-components.module';
+
 
 
 @NgModule({
@@ -54,20 +45,9 @@ import {MatDialogModule} from '@angular/material/dialog';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    NoopAnimationsModule,
-    MatSelectModule,
+    HttpClientModule,  
     ReactiveFormsModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTooltipModule,
-    MatTabsModule,
-    MatDialogModule,
+    MatComponentsModule,  
     RouterModule.forRoot([
       {
         path: '',
